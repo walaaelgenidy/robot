3d88f3aba18bb170ce7f10fbeb72eb997e41947b
walaaelgenidy <walaaelgenidy@gitlab.com>
2020-03-31 05:42:50 +0200

    Updates

---
+++ precache-manifest.3d3aa30c426cdb4214baa54b0a7dd760.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "adabb37a571fbfad07cb",
+    "url": "/robots_selector/static/css/main.d466eab1.chunk.css"
+  },
+  {
+    "revision": "adabb37a571fbfad07cb",
+    "url": "/robots_selector/static/js/main.78a03c6f.chunk.js"
+  },
+  {
+    "revision": "862092f32469b0381d24",
+    "url": "/robots_selector/static/js/runtime~main.74027db0.js"
+  },
+  {
+    "revision": "e466b70e484681b9a51e",
+    "url": "/robots_selector/static/css/2.4f22cdde.chunk.css"
+  },
+  {
+    "revision": "e466b70e484681b9a51e",
+    "url": "/robots_selector/static/js/2.1b9b2b06.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots_selector/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d7f11519acfc534168907d1886d46942",
+    "url": "/robots_selector/index.html"
+  }
+];
